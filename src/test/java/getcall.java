13b05f486b6b8558b2e500625b25f4c661c8e387
Rahman <aterahman@deloitte.com>
2022-04-12 07:02:30 +0530

    Added method to get rsponse payload and verify whther the genders are male or female

--- src/test/java/getcall.java
+++ src/test/java/getcall.java
@@ -1,3 +1,9 @@
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -7,8 +13,9 @@ import static org.hamcrest.Matchers.*;
 //class to get() payload
 public class getcall
 {
-    @Test
-    public void test_get_call()
+    //method to get the status code of the response payload
+    @Test(priority = 1)
+    public void test_statuscode()
     {
         given().
                 baseUri("https://gorest.co.in/public/v1").
@@ -18,5 +25,31 @@ public class getcall
         then().
                 statusCode(200);
 
+    }
+
+
+    //method to get the response payload and to verify whether all genders are male or female
+    @Test
+    public void verify_response_payload()
+    {
+        Response response =
+        given().
+                baseUri("https://gorest.co.in/public/v1").
+
+        when().
+                get("/users").
+        then().
+                extract().response();
+
+
+        JSONObject obj = new JSONObject(response.asString());
+
+        JSONArray arr = obj.getJSONArray("data");
+        for(int i =0;i<arr.length();i++)
+        {
+            String gender = (String) arr.getJSONObject(i).get("gender");
+            Assert.assertTrue(gender.equals("male")||gender.equals("female"));
+
+        }
     }
 }
