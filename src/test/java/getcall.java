9a88e2ae7557625ad5db8c71ccbdff612fb17302
Rahman <aterahman@deloitte.com>
2022-04-12 07:30:17 +0530

    added a method to verify if atleast 2 emails end with .biz, and added comments at places

--- src/test/java/getcall.java
+++ src/test/java/getcall.java
@@ -55,27 +55,53 @@ public class getcall
     @Test(priority = 3)
     public void verify_response_payload_gender()
     {
-
         for(int i =0;i<arr.length();i++)
         {
+            //stores the gender at location i in the array
             String gender = (String) arr.getJSONObject(i).get("gender");
+
+            //checks if the required condition is met
             Assert.assertTrue(gender.equals("male")||gender.equals("female"));
 
         }
     }
 
+    //method to check if atleast 2 emails have .biz
+    @Test(priority = 4)
+    public void verify_response_payload_emailextension()
+    {
+        int c=0;
+        for(int i = 0;i<arr.length();i++)
+        {
+            //stores the email at location i in the array
+            String email = (String) arr.getJSONObject(i).get("email");
+
+            //counts the number of emails ending with .biz
+            if(email.contains(".biz"))
+                c++;
+        }
+
+        //checks if the required condition is met
+        Assert.assertTrue(c>=2);
+
+    }
+
 
     //method to verify if the ids in the payload are unique
-    @Test(priority = 4)
+    @Test(priority = 5)
     public void verify_response_payload_id()
     {
-
         for(int i =0;i<arr.length();i++)
         {
+            //stores the id at location i in the array
             int id1 = (int) arr.getJSONObject(i).get("id");
+
             for(int j =i+1;j<arr.length();j++)
             {
+                //stores the id at location j in the array
                 int id2 = (int) arr.getJSONObject(j).get("id");
+
+                //checks if the required condition is met
                 Assert.assertFalse(id1==id2);
             }
 
