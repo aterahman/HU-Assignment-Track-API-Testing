9cc31ab71b1fad4e8a0b97f64a8c102681b82e12
Rahman <aterahman@deloitte.com>
2022-04-13 05:32:46 +0530

    made changes to the methods

--- src/test/java/postcall.java
+++ src/test/java/postcall.java
@@ -1,4 +1,9 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 
@@ -6,16 +11,24 @@ import static io.restassured.RestAssured.given;
 public class postcall
 {
     @Test(priority = 1)
-    public void test_statuscode()
+    public void test_statuscode()throws FileNotFoundException, IOException
     {
+        String temp_token = "1e3fbfc04d510423f16db6b1976fb8d49a339e4e486ef5006ebe118b552a2a32";
+        JSONArray arr;
+
+        excelcall e = new excelcall();
+        arr = e.excel();
+        JSONObject obj = arr.getJSONObject(0);
+
         given().
                 baseUri("https://gorest.co.in/public/v1").
-
-                when().
+                header("Authorization","Bearer"+ temp_token).
+                body(obj).
+                header("Content-type","applications/java").
+        when().
                 post("/users").
-                then().
-                statusCode(200);
-
+        then().
+                statusCode(201);
     }
 
     @Test(priority = 2)
@@ -23,6 +36,7 @@ public class postcall
     {
         given().
                 baseUri("https://gorest.co.in/public/v1").
+
 
                 when().
                 post("/users").
