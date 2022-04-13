3122607cf7d45490a1205056b6ee2d6db9d10c70
Rahman <aterahman@deloitte.com>
2022-04-13 09:24:34 +0530

    Class to generate extent report

--- src/test/java/extentgenerator.java
+++ src/test/java/extentgenerator.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class extentgenerator {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+//class to generate extent report
+public class extentgenerator
+{
+    public static ExtentReports extentReporter()
+    {
+        ExtentReports extentReports = new ExtentReports();
+        ExtentSparkReporter reporter = new ExtentSparkReporter("extent.html");
+        extentReports.attachReporter(reporter);
+        reporter.config().setTheme(Theme.DARK);
+        reporter.config().setDocumentTitle("GoRest Assignment Report");
+        return extentReports;
+    }
 }
