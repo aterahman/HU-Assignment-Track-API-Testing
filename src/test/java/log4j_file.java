9694d2ac679f7435a91f44933d2fe2e2a6d14b37
Rahman <aterahman@deloitte.com>
2022-04-13 09:23:52 +0530

    log4j initializer file

--- src/test/java/log4j_file.java
+++ src/test/java/log4j_file.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class log4j_file {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class log4j_file {
+    Logger logger = LogManager.getLogger(log4j_file.class.getName());
+
 }
